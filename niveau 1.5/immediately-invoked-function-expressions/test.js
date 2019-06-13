6b1173b8dd60968d525c26275ba0976a810a4ea7
evan-boissonnot <commercial@boissonnot.Fr>
2019-06-13 23:50:36 +0200

    final example to keep local scope

--- niveau 1.5/immediately-invoked-function-expressions/test.js
+++ niveau 1.5/immediately-invoked-function-expressions/test.js
@@ -1,9 +1,19 @@
 /// bad prob
 for(var index = 0 ; index < 5 ; index++) {
     setTimeout(function() {
-      console.log(index + "with side effect", index);
+        console.log(index + "with side effect", index);
     }, 1000);
-  }
+}
+
+function recreateLocalScope(localIndex) {
+    return function() {
+        console.log("no side effect", localIndex);
+    };
+}
+
+for(var index = 0 ; index < 5 ; index++) {
+    setTimeout(recreateLocalScope(index), 1000);
+}
 
 
 // Immediately invoked function expressions, aka IIFE
